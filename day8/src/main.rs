b33ff9e53c54078c65572153f63d1a240216a403
Bartosz Pietrzak <bartosz.pietrzak@ocado.com>
2025-12-18 11:18:21 +0100

    added structs

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,28 +1,41 @@
 use anyhow::{Context, Result};
 use std::fs::read_to_string;
 
+#[derive(Debug)]
 struct Jbox {
     x: usize,
     y: usize,
     z: usize,
 }
 
+impl Jbox {
+    fn distance_to(&self, other: &Jbox) -> f64 {
+        let dx = other.x as f64 - self.x as f64;
+        let dy = other.y as f64 - self.y as f64;
+        let dz = other.z as f64 - self.z as f64;
+        let sum_of_squares = dx * dx + dy * dy + dz * dz;
+        sum_of_squares.sqrt()
+    }
+}
+
 fn main() -> Result<()> {
     let path = "test";
     let file = read_to_string(path).context("Failed to read file to string")?;
-    let lines = file.lines();
-    let all_jboxes = lines
-        .filter_map(|line: &str| -> Result<Jbox> {
-            let coords = line
-                .split(",")
-                .map(|coord| coord.parse::<usize>().context("Invalid coordinate"));
-            Jbox {
-                x: coords.next().context("Missing x coorfinate")?,
-                y: coords.next().context("Missing y coorfinate")?,
-                z: coords.next().context("Missing z coorfinate")?,
-            }
+    let all_jboxes: Result<Vec<Jbox>> = file
+        .lines()
+        .map(|line: &str| -> Result<Jbox> {
+            let mut coords = line.split(",").map(|coord| -> Result<usize> {
+                let parsed_coord = coord.parse::<usize>().context("Invalid coordinate")?;
+                Ok(parsed_coord)
+            });
+            Ok(Jbox {
+                x: coords.next().context("Missing x coorfinate")??,
+                y: coords.next().context("Missing y coorfinate")??,
+                z: coords.next().context("Missing z coorfinate")??,
+            })
         })
-        .collect::<Result<Vec<Jbox>>>()?;
+        .collect();
+    println!("{:?}", all_jboxes);
 
     Ok(())
 }
